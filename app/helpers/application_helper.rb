f6cbda1ff214d74dc7c8a53eff009556383dff90
yukiuematsu <yukiuematsu@icloud.com>
2019-03-31 02:53:46 +0000

    Finish static page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,16 @@
 module ApplicationHelper
+  
+  #ページごとのタイトル
+  
+  def full_title(page_title="")  #メゾット名と引数定義
+    base_title="勤怠システム"    
+    #変数代入
+    
+    if page_title.empty?  #引数の論理値確認
+      base_title　　　　　　#page_titleからの時base_title返す
+    else
+       page_title+"|"+base_title #文字列結合
+    end    
+     
+  end
 end
